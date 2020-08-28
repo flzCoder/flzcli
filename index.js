96eec796e2b3d11298acfc69c4d261b36085a394
冯鲁哲 <fengluzhe@corp.netease.com>
2020-08-28 09:19:42 +0800

    del .git  &&  npm install

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 #!/usr/bin/env node
+require("shelljs/global");
+const path = require('path');
+const fs = require("fs");
 const program = require('commander');
 var process = require('child_process');
+
 program.version('1.0.5', '-v, --version')
     .command('init <name>')
     .action((name) => {
         console.log('clone template ...');
-        process.exec('git clone https://github.com/flzCoder/vue-template.git ' + name, function(error, stdout, stderr) {
+        process.exec('git clone https://github.com/flzCoder/vue-test.git ' + name, function(error, stdout, stderr) {
             if (error !== null) {
                 console.log('exec error: ' + error);
                 return;
             }
             console.log(stdout);
             console.log('clone success');
+            let dirname = path.resolve(__dirname,name);
+            cd(`${dirname}`)
+            console.log('rm rf .git');
+            exec(`rm -rf .git`)
+            console.log('npm install...');
+            exec(`npm install`)
+            
         });
     });
 program.parse(process.argv);
